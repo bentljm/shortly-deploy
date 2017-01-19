ccf185a3a7822aa703e32f06d89138959eae607c
Jeff Bentler <bentljm@gmail.com>
2017-01-19 11:31:12 -0800

    Finished concatenated files

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,9 +6,24 @@ module.exports = function(grunt) {
       options: {
         separator: ';\n'
       },
-      dist: {
-        src: ['public/lib/*.js', 'public/client/*.js'],
-        dest: 'public/dist/concat.js'
+      lib: {
+        src: ['public/lib/jquery.js',
+              'public/lib/underscore.js',
+              'public/lib/backbone.js',
+              'public/lib/handlebars.js',
+              ],
+        dest: 'public/dist/lib.js'
+      },
+      client: {
+        src: ['public/client/app.js',
+              'public/client/link.js',
+              'public/client/links.js',
+              'public/client/linkView.js',
+              'public/client/linksView.js',
+              'public/client/createLinkView.js',
+              'public/client/router.js'
+              ],
+        dest: 'public/dist/client.js'
       }
     },
 
