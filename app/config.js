a64c6af21953b2ef67757dc59571aac579423c6f
Jeff Bentler <bentljm@gmail.com>
2017-02-14 19:56:24 -0800

    Updated config file

--- app/config.js
+++ app/config.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 
+mongoose.Promise = require('bluebird');
+
 mongoose.connect('mongodb://localhost/shortlydb');
 
 var db = mongoose.connection;
@@ -9,43 +11,3 @@ db.once('open', function() {
 });
 
 module.exports = db;
-
-
-// var path = require('path');
-// var knex = require('knex')({
-//   client: 'sqlite3',
-//   connection: {
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   },
-//   useNullAsDefault: true
-// });
-// var db = require('bookshelf')(knex);
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('baseUrl', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
